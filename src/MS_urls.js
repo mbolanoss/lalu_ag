182c8710756c4712d2c4babae399438e4d1292bc
mrodrigueztr <mrodrigueztr@unal.edu.co>
2022-05-04 00:46:54 -0500

    updating Ms urls

--- src/MS_urls.js
+++ src/MS_urls.js
@@ -1,12 +1,12 @@
-const playlistURL = "http://localhost";
-const artistURL = "http://localhost";
-//const playlistMS_ip = ""
-//const artisttMS_ip = ""
+const playlistMS_ip = "http://localhost";
+const artistMS_ip = "http://localhost";
+
+
 const playlistMS_port = 3001;
-const playlistMS_url = `${playlistURL}:${playlistMS_port}/lalu/playlists`
+const playlistMS_url = `${playlistMS_ip}:${playlistMS_port}/lalu/playlists`
 
 const artistMS_port = 3005;
-const artistMS_url = `${artistURL}:${artistMS_port}/lalu/artist`
+const artistMS_url = `${artistMS_ip}:${artistMS_port}/lalu/artist`
 
 module.exports = {
     playlistMS_url,
